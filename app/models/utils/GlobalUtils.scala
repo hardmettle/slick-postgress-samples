08713a7a8c59087850530965e912c53be7703f73
hardmettle <harsh2005_7@yahoo.com>
2014-08-14 16:41:34 +0530

    formatting of file

--- app/models/utils/GlobalUtils.scala
+++ app/models/utils/GlobalUtils.scala
@@ -9,10 +9,16 @@ import users.User
 /**
  * Created by harsh on 10/8/14.
  */
-case class DateMetaData(createdOn:DateTime,modifiedOn:Option[DateTime])
-case class UserMetaData(createdBy:Long,modifiedBy:Option[Long])
-case class Media(media_id:Long,url:String,local_path:Option[String],title:String,media_type:MediaType,dates:DateMetaData)
-case class DateTimeFormat(dateformat:DateFormat,timeformat:TimeFormat,timezone:String)
-case class TollFreeNumber(primary:String,secondary:Option[String])
-case class WorkTimings(start:DateTime,end:DateTime)
-case class Size(width:Int,height:Int)
+case class DateMetaData(createdOn: DateTime, modifiedOn: Option[DateTime])
+
+case class UserMetaData(createdBy: Long, modifiedBy: Option[Long])
+
+case class Media(media_id: Long, url: String, local_path: Option[String], title: String, media_type: MediaType, dates: DateMetaData)
+
+case class DateTimeFormat(dateformat: DateFormat, timeformat: TimeFormat, timezone: String)
+
+case class TollFreeNumber(primary: String, secondary: Option[String])
+
+case class WorkTimings(start: DateTime, end: DateTime)
+
+case class Size(width: Int, height: Int)
